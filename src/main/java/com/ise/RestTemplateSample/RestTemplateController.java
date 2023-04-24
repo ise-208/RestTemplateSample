4dcdc6ac32881ac0ea9a74cd85525913e8ca9344
ise-208 <seed.0ri00@gmail.com>
2023-04-24 23:56:31 +0900

    controllerの作成

---
+++ src/main/java/com/ise/RestTemplateSample/RestTemplateController.java
@@ -0,0 +1,20 @@
+package com.ise.RestTemplateSample;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@RestController
+public class RestTemplateController {
+    Logger log = LoggerFactory.getLogger(RestTemplateController.class);
+
+    ExchangeSample exchangeSample = new ExchangeSample();
+
+    @GetMapping("/hello")
+    public void hello() {
+        log.info("helloの実施");
+        HttpEntity<String> request = exchangeSample.request;
+    }
+}
